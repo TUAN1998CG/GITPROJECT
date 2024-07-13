84aea9244a89b58fbfa4a3f320d6a64916c739af
albumanh1234 <albumanh1234@gmail.com>
2024-07-13 20:53:07 +0700

    ss7/

---
+++ ss7/thuc_hanh/bai4.js
@@ -0,0 +1,6 @@
+let inputWidth=prompt('Enter the width');
+let inputHeigth=prompt('Enter the height');
+let width = parseInt(inputWidth)
+let height=parseInt(inputHeigth)
+let area =width*height;
+document.write('The area =' + area);
